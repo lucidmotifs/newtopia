c2a217054adbaa9a866c0f488826e8e662ee6424
Paul Cooper <p.cooper@mac.com>
2016-11-29 21:46:50 +0100

    registered Army object with admin app

--- newtopia/ntgame/admin.py
+++ newtopia/ntgame/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Kingdom, Province
+from .models import Kingdom, Province, Army
 
 # Register your models here.
 admin.site.register(Kingdom)
 admin.site.register(Province)
+admin.site.register(Army)
