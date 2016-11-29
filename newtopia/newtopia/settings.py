5c6b6a1a692aeddb84997f45d06f7dc693e20609
Paul Cooper <p.cooper@mac.com>
2016-11-29 20:29:53 +0100

    adding RDS config

--- newtopia/newtopia/settings.py
+++ newtopia/newtopia/settings.py
@@ -74,17 +74,28 @@ WSGI_APPLICATION = 'newtopia.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'newtopia',
-        'USER': 'newtopiadb',
-        'PASSWORD': 'specialphrase42',
-        'HOST': '192.168.0.42',
-        'PORT': '5432',
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'newtopia',
+            'USER': 'newtopiadb',
+            'PASSWORD': 'specialphrase42',
+            'HOST': '192.168.0.42',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
