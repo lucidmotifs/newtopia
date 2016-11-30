c45577af4f9abe5e7758023d66617e4ddc8cb327
Paul Cooper <p.cooper@mac.com>
2016-12-01 00:57:49 +0100

    Adding meta object for game Rules

--- newtopia/ntgame/admin.py
+++ newtopia/ntgame/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from .models import Kingdom, Province, Army, Race
+from .models import Kingdom, Province, Army, Race, Rule
 
 # Register your models here.
+
+# Game Entities
 admin.site.register(Kingdom)
 admin.site.register(Province)
 admin.site.register(Army)
 admin.site.register(Race)
+
+# Meta Objects
+#@admin.register(Rule)
+class RuleAdmin(admin.ModelAdmin):
+    class Meta:
+        app_label = "ntmeta"
+
+admin.site.register(Rule)
