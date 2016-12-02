50c9caea5bd7d58e7e21ae19d6162dd1174042b2
Paul Cooper <p.cooper@mac.com>
2016-12-02 17:40:47 +0100

    Setting up S3 static file storage.
    Will create dev rule to ignore these settings once working for easier
    dev.

--- newtopia/newtopia/settings.py
+++ newtopia/newtopia/settings.py
@@ -27,6 +27,34 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+os.environ['S3_USE_SIGV4'] = 'True'
+
+AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-eu-central-1-482995622259'
+AWS_ACCESS_KEY_ID = 'AKIAIVN3X4JPYMUJ6EQA'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_S3_HOST = 's3.eu-central-1.amazonaws.com'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# The location attribute being set to static will put all our file in the folder static/
+STATICFILES_LOCATION = 'static'
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+
+# Media Files (uploads)
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 # Application definition
 
@@ -37,6 +65,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'ntgame',
     'ntmeta'
 ]
@@ -136,3 +165,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+#MEDIA FILE (user uploaded files)
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
+MEDIA_URL = '/media/'
