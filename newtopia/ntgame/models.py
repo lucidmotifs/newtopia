d2acad466c4d9128ab3bcc5c75d67213800184ad
Paul Cooper <p.cooper@mac.com>
2017-04-07 21:18:33 +0200

    Adding proper Offense Points and Defense Points calculations.

--- newtopia/ntgame/models.py
+++ newtopia/ntgame/models.py
@@ -64,11 +64,20 @@ class Military(models.Model):
     elites = models.IntegerField(default=0)
     thieves = models.IntegerField(default=0)
 
+    @property
+
 
     def total_off_points(self, race):
+        # base offense
+        raw_offense = (self.soldiers * 1) + \
+            (self.offspec * self.province.race.offense_spec_value) + \
+            (self.elites * self.province.race.elite_offense)
+
+
         return  (self.soldiers * 1) + \
-                (self.offspec * race.offense_spec_value) + \
-                (self.elites * race.elite_offense)
+                (self.offspec * self.province.race.offense_spec_value) + \
+                (self.elites * self.province.race.elite_offense) + \
+                (self.province.war_horses )
 
 
     def total_def_points(self, race):
