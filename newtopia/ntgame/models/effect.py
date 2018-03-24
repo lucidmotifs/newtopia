641ea14a754f632a970970668516735ccf7bda63
Paul Cooper <lucidmotifs@gmail.com>
2018-03-24 01:31:23 +0100

    update effect models' attributes

--- newtopia/ntgame/models/effect.py
+++ newtopia/ntgame/models/effect.py
@@ -1,6 +1,10 @@
+# python modules
+from enum import Enum
+
 # django modules
 from django.db import models
 
+# nt modules
 from .province import Province
 from .infrastructure import Building
 
@@ -12,10 +16,14 @@ class Effect(models.Model):
     with a given magnitude would impact how fast peasants grow per turn."""
     name = models.CharField(max_length=40,unique=False)
 
-    """ Code used to identify the effect, like a key. """
-    tag = models.CharField(max_length=40,unique=True)
+    """ The entity that generated the effect """
+    entity = models.ForeignKey('ntmeta.Entity',
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False)
 
-    # others to be implemented
+    """ Code used to identify the effect, like a key. HASH? """
+    tag = models.CharField(max_length=40,unique=True)
 
     def __str__(self):
         return self.name
@@ -24,14 +32,49 @@ class Effect(models.Model):
 class EffectInstance(models.Model):
     """ An instance of an effect that can be applied to a building or spell. """
 
+    class EffectType(Enum):
+        DELAYED = 1
+        IMMEDIATE = 2     
+        OVER_TIME = 3
+        NEXT_TURN = 4
+
     """ The related effect """
     effect = models.ForeignKey(Effect,
         on_delete=models.CASCADE,
         null=False,
         blank=False)
 
-    """ Size of the effect. """
+    """ Determines the type of application produced """
+    effect_type = models.IntegerField(
+        choices=[e.value for e in EffectType], default=EffectType.IMMEDIATE)
+
+    """ How long effect persists. Ignore when `effect_type` is immediate and
+        determines when the delayed effect pops when `effect_type` is
+        DELAYED. Measured in ntdays """
+    duration = models.IntegerField(deault=1)
+
+    """ Size of the effect. Set to 100 if using raw value. """
     magnitude = models.FloatField(default=0.0)
+
+    """ Raw value increase/decrease will be converted to a percentage
+        if used with a subentity, such as a growth rate.
+        When Provided, magnitude will only be applied to the raw_value.
+        Exception: can be used as minimum value if base_is_min == True """
+    base_value = models.IntegerField(default=None)
+
+    """ When True, magnitude works as usual, and base_value is only applied if
+        the resulting Application value would be less than the base_value """
+    base_is_min = models.BooleanField(defaul=False)
+
+    """ Denotes negative or positive version of effect """
+    is_negative = models.BooleanField(default=False)
+
+
+    def apply(self, province):
+        app = EffectApplication()
+        app.instance = self
+        app.province = province
+        app.
 
 
     def __str__(self):
@@ -50,17 +93,16 @@ class EffectApplication(models.Model):
         null=False,
         blank=False)
 
-    """ Type of effect; alters how the effect is applied. """
-    EFFECT_TYPE_CHOICES = (
-        (1, 'immediate'),
-        (2, 'each_round'),
-        (3, 'one_round'),
-        (4, 'on_expire'),
-        (5, 'permanent'),
-    )
-    etype = models.IntegerField(choices=EFFECT_TYPE_CHOICES, default=1)
+    applied_by = models.ForeignKey(Province,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False)
 
-    # Round the effect was applied
+    """ Type of effect; alters how the effect is applied. """
+    # Round the effect was applied (ntdate)
     applied_on = models.IntegerField()
-    # Round the effect expires (NULL permanent)
-    expires_at = models.IntegerField()
+    # Round the effect expires (ntdate) (NULL permanent, immediate)
+    expires_at = models.IntegerField(default=None)
+    # Round the effect is applied (ntdate)
+    # (NULL immediate, 0 every tick till expires)
+    applies_at = models.IntegerField(default=None)
