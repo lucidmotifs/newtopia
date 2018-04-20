6fb14fde9bef4cf203c5e771fe9a4f3e3f0ba768
Paul Cooper <p.cooper@mac.com>
2018-04-20 19:52:41 +0200

    ntgame: reworked effect related models

--- newtopia/ntgame/models/effect.py
+++ newtopia/ntgame/models/effect.py
@@ -14,7 +14,7 @@ class Effect(models.Model):
 
     """ e.g. Peasant Growth - would signify that applying this effect,
     with a given magnitude would impact how fast peasants grow per turn."""
-    name = models.CharField(max_length=40,unique=False)
+    name = models.CharField(max_length=40, unique=False)
 
     """ The entity that generated the effect """
     entity = models.ForeignKey('ntmeta.Entity',
@@ -23,13 +23,13 @@ class Effect(models.Model):
         blank=False)
 
     """ Code used to identify the effect, like a key. HASH? """
-    tag = models.CharField(max_length=40,unique=True)
+    tag = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
 
 
-class EffectInstance(models.Model):
+class Instance(models.Model):
     """ An instance of an effect that can be applied to a building or spell. """
 
     class EffectType(Enum):
@@ -51,7 +51,7 @@ class EffectInstance(models.Model):
     """ How long effect persists. Ignore when `effect_type` is immediate and
         determines when the delayed effect pops when `effect_type` is
         DELAYED. Measured in ntdays """
-    duration = models.IntegerField(deault=1)
+    duration = models.IntegerField(default=1)
 
     """ Size of the effect. Set to 100 if using raw value. """
     magnitude = models.FloatField(default=0.0)
@@ -69,21 +69,19 @@ class EffectInstance(models.Model):
     """ Denotes negative or positive version of effect """
     is_negative = models.BooleanField(default=False)
 
-
     def apply(self, province):
-        app = EffectApplication()
+        app = Application()
         app.instance = self
         app.province = province
-        app.
 
 
     def __str__(self):
         return "{} with mag. {}".format(self.effect.name, self.magnitude)
 
 
-class EffectApplication(models.Model):
+class Application(models.Model):
     """ Used to apply effects to provinces """
-    instance = models.ForeignKey(EffectInstance,
+    instance = models.ForeignKey(Instance,
         on_delete=models.CASCADE,
         null=True,
         blank=True)
